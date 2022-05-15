7d21a142341fc622f5e59db37475fea3540e310b
SokolyukMisha <sokolyukmisha@gmail.com>
2022-05-15 11:23:57 +0300

    implementation.go

--- implementation.go
+++ implementation.go
@@ -1,11 +1,13 @@
 package lab2
 
+import "strings"
+
 func Reverse(s string) string {
-    runes := []rune(s)
+    runes := strings.Split(s, " ")
     for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
         runes[i], runes[j] = runes[j], runes[i]
     }
-    return string(runes)
+    return strings.Join(runes, " ")
 }
 
 // TODO: document this function.
