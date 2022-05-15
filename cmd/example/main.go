39b995c8f53bc0461bda54d7a6fc1b3f3379bcdc
juliion <juliaionova111@gmail.com>
2022-05-15 12:02:36 +0300

    Add flag handling that gets the file for the result

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -29,6 +29,16 @@ func main() {
 		defer file.Close()
 		handler.Input = file
 	}
+	if *outputFile != "" {
+		file, err := os.OpenFile(*outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		handler.Output = file
+	} else {
+		handler.Output = os.Stdout
+	}
 
 	res, _ := lab2.PrefixToPostfix("+ 2 2")
 	fmt.Println(res)
