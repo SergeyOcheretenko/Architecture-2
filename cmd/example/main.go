4a79757e3a33e61737c9e29b0e65421638a9cae6
juliion <juliaionova111@gmail.com>
2022-05-15 11:54:59 +0300

    Add handling of received flags that pass the input expression

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
+	"os"
+	"log"
 	lab2 "github.com/roman-mazur/architecture-lab-2"
 )
 
@@ -15,13 +18,17 @@ var (
 func main() {
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	handler := &lab2.ComputeHandler{}
+	if *inputExpression != "" {
+		handler.Input = strings.NewReader(*inputExpression)
+	} else {
+		file, err := os.Open(*inputFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		handler.Input = file
+	}
 
 	res, _ := lab2.PrefixToPostfix("+ 2 2")
 	fmt.Println(res)
